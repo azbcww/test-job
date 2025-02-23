b5db2afa889e3792d847b350ff651fb973e1b942
WNomunomu <kohsei.nmr@gmail.com>
2025-02-23 19:33:38 +0000

    install contribution component temporary

--- frontend/src/app/[user-name]/page.tsx
+++ frontend/src/app/[user-name]/page.tsx
@@ -6,6 +6,8 @@
 import { AchievementCard } from "@/components/AchievementCard";
 import { LevelCard } from "@/components/LevelCard";
 
+import { ContributionCalendar } from "react-contribution-calendar";
+
 export default function UserHome({ params }: { params: { user_name: string } }) {
   // const router = useRouter();
   const user_name = params.user_name;
@@ -16,6 +18,21 @@ export default function UserHome({ params }: { params: { user_name: string } }) 
     { category: "infrastructure", level: 8 },
     { category: "CI/CD", level: 6 }
   ];
+
+  const contributionData = [
+    {
+      '2023-04-20': { level: 2 }
+    },
+    {
+      '2023-07-08': { level: 1 },
+    },
+    {
+      '2023-07-09': { level: 4 },
+    },
+    {
+      '2024-03-31': { level: 3 }
+    },
+  ]
 
   // useEffect(() => {
   //   const storedUser = localStorage.getItem("user");
@@ -34,6 +51,28 @@ export default function UserHome({ params }: { params: { user_name: string } }) 
         category="book"
       />
       <LevelCard LevelInfoArray={levelData} />
+      <ContributionCalendar
+        data={contributionData}
+        dateOptions={{
+          start: '2023-04-04',
+          end: '2024-04-04',
+          // daysOfTheWeek: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+          startsOnSunday: true,
+          includeBoundary: true,
+        }}
+        styleOptions={{
+          theme: 'grass',
+          cx: 10,
+          cy: 10,
+          cr: 2,
+        }}
+        // visibilityOptions={{
+        //   hideDescription: false,
+        //   hideMonthLabels: false,
+        //   hideDayLabels: false,
+        // }}
+        scroll={false}
+      />
     </div>
   );
 }
