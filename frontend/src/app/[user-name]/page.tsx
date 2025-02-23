5e8694b58b593249a94d9644688498f79d082b38
WNomunomu <kohsei.nmr@gmail.com>
2025-02-23 15:36:57 +0000

    fix lint

--- frontend/src/app/[user-name]/page.tsx
+++ frontend/src/app/[user-name]/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+// import { useEffect } from "react";
+// import { useRouter } from "next/navigation";
 
 export default function UserHome({ params }: { params: { user_name: string } }) {
-  const router = useRouter();
+  // const router = useRouter();
   const user_name = params.user_name;
 
   // useEffect(() => {
