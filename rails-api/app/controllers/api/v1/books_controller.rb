cfb92bafbc3097242f355b56805a13aa1fe60aae
azbcww <zaima.teppei@gmail.com>
2025-02-24 21:32:43 +0900

    book api

--- rails-api/app/controllers/api/v1/books_controller.rb
+++ rails-api/app/controllers/api/v1/books_controller.rb
@@ -5,6 +5,23 @@ module Api
         books = Book.all
         render json: books
       end
+
+      def create
+        book = Book.new(book_params)
+
+        if book.save
+          render json: book, status: :created
+        else
+          render json: { errors: book.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      # Strong Parameters を使って、許可されたパラメータのみを受け付ける
+      def book_params
+        params.require(:book).permit(:title, :author, :total_pages, :isbn, :image)
+      end
     end
   end
 end
--- rails-api/db/schema.rb
+++ rails-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_23_123311) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_24_105913) do
   create_table "book_categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "book_id", null: false
     t.bigint "category_id", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_23_123311) do
     t.integer "total_pages"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "isbn"
   end
 
   create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
