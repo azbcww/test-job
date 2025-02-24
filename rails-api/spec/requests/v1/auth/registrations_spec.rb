d43d5cd63b6daad71ccf06ca69bbf9ac7ae347c2
abichan99911111 <shinei-a@weseek.co.jp>
2025-02-24 10:21:02 +0900

    auth spec

--- rails-api/spec/requests/v1/auth/registrations_spec.rb
+++ rails-api/spec/requests/v1/auth/registrations_spec.rb
@@ -1,38 +1,115 @@
 require 'rails_helper'
 
 RSpec.describe "V1::Auth::Registrations", type: :request do
-  describe "GET /v1/auth/registrations" do
-    it "works! (now write some real specs)" do
-      request_params = {
-        email: "a@a.com",
-        password: "password",
-        password_confirmation: "password",
+  describe "POST api/v1/auth" do
+    subject {  post "api/v1/auth", headers: { 'Content-Type': 'application/json' }, params: request_params.to_json}
+    
+    describe "成功時" do
+      let(:request_params) { 
+        { 
+          email: "a@a.com", 
+          password: "password", 
+          password_confirmation: "password" 
+        } 
       }
-      post "/v1/auth", headers: { 'Content-Type': 'application/json' }, params: request_params.to_json
 
+      it "ユーザー登録ができる" do
+      expect { subject }.to change { User.count }.by(1)
+      end
+
+      it "200レスポンスが返ってくる" do
+      subject
       expect(response).to have_http_status(200)
+      end
+    end
+
+    describe "失敗時" do
+      context "request params が無効な場合" do
+        let(:request_params) { { email: nil, password: "password", password_confirmation: "password" } }
+
+        it "ユーザー登録ができない" do
+          expect { subject }.not_to change { User.count }
+        end
+
+        it "422レスポンスが返ってくる" do
+          subject
+          expect(response).to have_http_status(422)
+        end
+      end
     end
   end
 
   describe "POST /v1/auth/sign_in" do
+    subject {post "api/v1/auth/sign_in", headers: { 'Content-Type': 'application/json' }, params: sign_in_params.to_json}
+    
     before do
       request_params = {
         email: "a@a.com",
         password: "password",
         password_confirmation: "password",
       }
-      post "/v1/auth", headers: { 'Content-Type': 'application/json' }, params: request_params.to_json
+      post "api/v1/auth", headers: { 'Content-Type': 'application/json' }, params: request_params.to_json
     end
 
-    it "signs in the user" do
-      sign_in_params = {
-        email: "a@a.com",
-        password: "password"
+    describe "成功時" do
+      let(:sign_in_params) {
+        {
+          email: "a@a.com",
+          password: "password"
+        }
       }
-      post "/v1/auth/sign_in", headers: { 'Content-Type': 'application/json' }, params: sign_in_params.to_json
+      it "signs in the user" do
+        subject
+        expect(response).to have_http_status(200)
+        expect(response.headers).to include('access-token', 'client', 'uid')
+      end
+    end
+    
+    describe "失敗時" do
+      let(:sign_in_params) {
+      {
+        email: "a@a.com",
+        password: "wrongpassword"
+      }
+      }
+      it "does not sign in the user" do
+        subject
+        expect(response).to have_http_status(401)
+        expect(response.headers).not_to include('access-token', 'client', 'uid')
+      end
+    end
+  end
 
-      expect(response).to have_http_status(200)
-      expect(response.headers).to include('access-token', 'client', 'uid')
+  describe "DELETE /v1/auth/sign_out" do
+    subject {delete "api/v1/auth/sign_out", headers: sign_out_header}
+
+    describe "成功時" do
+      let(:sign_out_header) {
+        {
+        }
+      }
+      it "signs out the user" do
+        res = register_and_sign_in("a@a.com", "password")
+        sign_out_header["client"] = res["client"]
+        sign_out_header["access-token"] = res["access-token"]
+        sign_out_header["uid"] = res["uid"]
+
+        subject
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe "ヘッダが正しくないとき" do
+      let(:sign_out_header) {
+        {
+        }
+      }
+      it "signs out the user" do
+        register_and_sign_in("a@a.com", "password")
+
+        subject
+        expect(response).to have_http_status(404)
+      end
     end
   end
 end
---
+++ rails-api/spec/support/auths_helper.rb
@@ -0,0 +1,24 @@
+module AuthsHelper
+  def register_and_sign_in(email, password)
+    request_params = {
+        email: email,
+        password: password,
+        password_confirmation: password,
+      }
+    post "api/v1/auth", headers: { 'Content-Type': 'application/json' }, params: request_params.to_json
+
+    post "api/v1/auth/sign_in/",
+      params: { email: email, password: password },
+      as: :json
+
+    response.headers.slice('client', 'access-token', 'uid')
+  end
+
+  def sign_in(user)
+    post "api/v1/auth/sign_in/",
+      params: { email: user[:email], password: user[:password] },
+      as: :json
+
+    response.headers.slice('client', 'access-token', 'uid')
+  end
+end
