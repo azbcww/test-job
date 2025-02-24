c6708fa79349d1f6e82fb4dd1009fb4217dbded2
WNomunomu <kohsei.nmr@gmail.com>
2025-02-24 20:54:59 +0000

    fix rspec

--- rails-api/app/controllers/api/v1/registrations_controller.rb
+++ rails-api/app/controllers/api/v1/registrations_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class RegistrationsController < DeviseTokenAuth::RegistrationsController
-      skip_before_action :authenticate_api_v1_user!
+      # skip_before_action :authenticate_api_v1_user!
       # after_action :set_token_info, only: [:create]
 
       # private
