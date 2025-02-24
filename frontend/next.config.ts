1693777b33bf1ba19f74e1503e2572c951f48f0e
WNomunomu <kohsei.nmr@gmail.com>
2025-02-24 09:03:12 +0000

    add next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["placehold.co"], // 外部画像のドメインを許可
+  },
 };
 
 export default nextConfig;
