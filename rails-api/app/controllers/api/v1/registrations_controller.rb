a7bf6a7e36c795235e812f65bb60c49b65391f5c
WNomunomu <kohsei.nmr@gmail.com>
2025-02-24 19:55:10 +0000

    settings  for auth

--- rails-api/app/controllers/api/v1/registrations_controller.rb
+++ rails-api/app/controllers/api/v1/registrations_controller.rb
@@ -1,6 +1,22 @@
 module Api
   module V1
     class RegistrationsController < DeviseTokenAuth::RegistrationsController
+      skip_before_action :authenticate_api_v1_user!
+      # after_action :set_token_info, only: [:create]
+
+      # private
+
+      # def sign_up_params
+      #   params.permit(:email, :password, :password_confirmation, :name)
+      # end
+
+      # def set_token_info
+      #   return unless @resource.persisted?
+
+      #   token = @resource.create_new_auth_token
+      #   response.set_header('access-token', token['access-token'])
+      #   response.set_header('client', token['client'])
+      # end
     end
   end
 end
--- rails-api/app/controllers/application_controller.rb
+++ rails-api/app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
-        include DeviseHackFakeSession
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseHackFakeSession
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :authenticate_api_v1_user!
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password, :session])
+  end
 end
